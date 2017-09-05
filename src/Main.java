9cf564b70d3a4bbb955b2fe854a876a7b12d0bc2
Bla bla <bla@bla.ru>
2017-09-05 09:20:48 +0600

    4 task isprav

--- src/Main.java
+++ src/Main.java
@@ -13,16 +13,28 @@ System.out.println("Hello world");
         float a = in.nextFloat();
         float b = in.nextFloat();
         float c = in.nextFloat();
-
+        float x1, x2;
         d=(float)(b*b-4*a*c);
-       if (d>0)
-        System.out.print ("2 корня");
+        if (a==b && a==c && a==0){
+            System.out.print ("Бесконечное множество");}
+            else {
+       if (d>0) {
+        System.out.print ("2 корня   ");
+       x1=(-b+(float)Math.sqrt (d))/2/a;
+           x2=(-b-(float)Math.sqrt (d))/2/a;
+           System.out.print (x1 +"    "+ x2);
+       }
         else
-       if (d==0)
-        System.out.print ("1 корень");
+       if (d==0) {
+        System.out.print ("1 корень   ");
+       x1=-b/2/a;
+           System.out.print (x1);
+       }
         else
         System.out.print ("нет корней");
 
+        }
+        in.close();//рассмотреть случай, когда не квадратное
     }
 
 }
