3d88d59b693b3f547ede0aa1280f7d570173c23e
Bla bla <bla@bla.ru>
2017-09-05 07:51:35 +0600

    second task

--- src/Main.java
+++ src/Main.java
@@ -1,10 +1,46 @@
 /**
  * Created by Student on 04.09.17.
  */
+import java.util.Scanner;
 public class Main {
     public static void main (String [] args){
 
 
 System.out.println("Hello world");
+float sum, srd, d;
+        Scanner in = new Scanner(System.in);
+        float a = in.nextFloat();
+        float b = in.nextFloat();
+        float c = in.nextFloat();
+sum=a+b+c;
+        srd=sum/3;
+        while (b>a || c>a ||c>b) {
+        if (b>a) {
+            d=a;
+            a=b;
+            b=d;
+        }
+            if (c>a) {
+                d=a;
+                a=c;
+                c=d;
+            }
+            if (c>b) {
+                d=b;
+                b=c;
+                c=d;
+            }
+        }
+        System.out.println ("Sum");
+        System.out.println (sum );
+        System.out.println ("Sre" );
+        System.out.println (srd );
+        System.out.println ("Chis" );
+        System.out.print (c);
+        System.out.print ("   ");
+        System.out.print (b);
+        System.out.print ("   ");
+        System.out.print (a);
     }
+
 }
