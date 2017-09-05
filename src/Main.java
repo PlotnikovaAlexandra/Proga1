366c531778855be17212302bba8394352cc82103
Bla bla <bla@bla.ru>
2017-09-05 08:32:35 +0600

    5 task

--- src/Main.java
+++ src/Main.java
@@ -7,41 +7,20 @@ public class Main {
 
 
 System.out.println("Hello world");
-int sum, d;
-        float srd;
-        Scanner in = new Scanner(System.in);
-        int a = in.nextInt();
-        int b = in.nextInt();
-        int c = in.nextInt();
-sum=a+b+c;
-        srd=(float) sum/3;
-        while (b>a || c>a ||c>b) {
-        if (b>a) {
-            d=a;
-            a=b;
-            b=d;
-        }
-            if (c>a) {
-                d=a;
-                a=c;
-                c=d;
-            }
-            if (c>b) {
-                d=b;
-                b=c;
-                c=d;
-            }
-        }
-        System.out.println ("Sum");
-        System.out.println (sum );
-        System.out.println ("Sre" );
-        System.out.println (srd );
-        System.out.println ("Chis" );
-        System.out.print (c);
-        System.out.print ("   ");
-        System.out.print (b);
-        System.out.print ("   ");
-        System.out.print (a);
-    }
 
+        float  d;
+        Scanner in = new Scanner(System.in);
+        float a = in.nextFloat();
+        float b = in.nextFloat();
+        float h = in.nextFloat();
+if (a<=b)
+{
+for (float i=a; i<=b; i+=h )
+{
+    d=(float) Math.sin (i);
+
+        System.out.println ("sin ("+i+")="+d );
+}
+    }//дописать вывод
+    }
 }
